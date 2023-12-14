fa18ba36bd7a8f94ed51aa9308650ac0dfcd7e30
Daisy <zilan.daisyye@gmail.com>
2023-12-14 18:45:20 -0400

    rotation works
    everything is in variable so changing numerical value wont effect the additive rotation angle while player is jumping

--- src/Player.java
+++ src/Player.java
@@ -18,23 +18,28 @@ import static com.sun.java.accessibility.util.AWTEventMonitor.addMouseListener;
 
 
 public class Player{
-
-//    private int x, y;
+    // hit box
     private double x, y;
-    private int width, height; // hitbox
-    private int width2, height2; // sprite
-    private double g = 2;
-    private double vy =0;
+    private int width, height;
 
-//    private double rotation = Math.toRadians(45);
+    // sprite
+    private int width2, height2;
+
+    // vector
+    private double g = 2; //gravity
+    private double vy = 0;
+    private double vx = 5;
+    private double initY = -30;
+
     double locX = width/2;
     double locY = height/2;
-//    AffineTransform tx = AffineTransform.getRotateInstance(rotation, locX, locY);
 
     private int floor=325;
 
-    private int vx;
-    private double rotation;
+    // rotation
+    private double angle = 0;
+    private double jumpRotate = (double) ( -Math.PI * g ) / ( 4 * initY ); // add to angle when jump
+
     private double ang = 0;
 
     private String gamemode;
@@ -50,50 +55,42 @@ public class Player{
 
     public Player(double x, double y, int width, int height) {
         this.gamemode = "cube";
+
         this.y=y;
         this.x = x;
+
         this.width  = width;
         this.height = height;
-        this.inAir = false;
-        //"assets/icons/Cube001.png"
 
+        this.inAir = false;
 
         this.icon = Util.resize(Util.loadBuffImage("assets/icons/Cube001.png"), width, height);
 
     }
 
-    public void rotate(){
-        double radians = Math.toRadians(45);
-        double sin = Math.abs(Math.sin(radians));
-        double cos = Math.abs(Math.cos(radians));
-        int newWidth = (int) Math.round(icon.getWidth() * cos );
-        double rotationPX = width/2;
-    }
 
     public void move() {
-            y += vy;
-            vy += g;
-            fallCheck();
-            System.out.println("Vel Y: " + vy);
-            if( y < floor) {
-                x += 5;
-            }
+        y += vy;
+        vy += g;
+        fallCheck();
+        if( y < floor) {
+            x += vx;
+            angle += jumpRotate;
+        }
     }
+
+
     public void fallCheck() {
         if ( y > floor ) {
             y = floor;
             vy = 0;
-            ang = 0;
-        }
-        else {
-            ang += vy;
         }
     }
 
 
     public void thrust() {
         if( y == floor) {
-            vy = -30;
+            vy = initY; //initial velocity added
         }
     }
 
@@ -106,27 +103,26 @@ public class Player{
     }
 
     public Polygon getOutline() {
-        return new Polygon(new int[] {(int) outlinePA.getX(), (int) outlinePB.getX(), (int) outlinePC.getX(), (int) outlinePD.getX()},
-                new int[] {(int) outlinePA.getY(), (int) outlinePB.getY(), (int) outlinePC.getY(), (int) outlinePD.getY()}, 4);
+        return new Polygon(new int[]
+                {(int) outlinePA.getX(), (int) outlinePB.getX(), (int) outlinePC.getX(), (int) outlinePD.getX()},
+                new int[] {(int) outlinePA.getY(), (int) outlinePB.getY(), (int) outlinePC.getY(), (int) outlinePD.getY()},
+                4);
     }
 
 
     public void draw(Graphics g) {
-//
-//        g.drawImage(icon, (int)x-width/2, (int)y-height/2, width, height, null);
-//
-//        drawHitbox(g);
+        System.out.println("angle: "+ angle);
 
         g.setColor(new Color(110,110,222));
 //        g.fillRect(0,0,800,600);
-
-
-//        ang += 0.0125;
+//        g.drawImage(icon, (int)x-width/2, (int)y-height/2, width, height, null);
+//        drawHitbox(g);
 
         AffineTransform rot = new AffineTransform();
-        rot.rotate(ang,(int)width/2,(int)height/2);
 
-        AffineTransformOp rotOp = new AffineTransformOp(rot, AffineTransformOp.TYPE_BILINEAR); 	// The options are: TYPE_BICUBIC, TYPE_BILINEAR, TYPE_NEAREST_NEIGHBOR
+        rot.rotate(angle,(double) width/2,(double) height/2);
+        AffineTransformOp rotOp = new AffineTransformOp(rot, AffineTransformOp.TYPE_BILINEAR);
+        // The options are: TYPE_BICUBIC, TYPE_BILINEAR, TYPE_NEAREST_NEIGHBOR
         Graphics2D g2D = (Graphics2D)g; 														// NEAREST_NEIGHBOR is fastest but lowest quality
         g2D.drawImage(icon,rotOp,(int)x-width/2,(int)y-height/2);
 
@@ -146,10 +142,7 @@ public class Player{
 //        g.drawRect((int) pos.getX(), (int) pos.getY(), width2, height2);
 //    }
 
-    public void setX(int x) {
-        this.x = x;
-    }
-
+    public void setX(int x) { this.x = x;}
 
     public void setY(int y) {
         this.y = y;
@@ -161,15 +154,12 @@ public class Player{
     public int getHeight() {return height;}
     public double getX() {return x;}
     public double getY() {return y;}
-    public int getVX() {return vx;}
+    public double getVX() {return vx;}
     public double getVY() {return vy;}
 
-    public String getGamemode() {
-        return gamemode;
-    }
+    public String getGamemode() { return gamemode; }
 
-    public void setGamemode(String e) {
-        gamemode = e;
+    public void setGamemode(String e) { gamemode = e;
     }
 
 }

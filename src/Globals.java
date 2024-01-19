11d738ee51e6c1d303e00507695ce9429cff8318
Kevin <realkevinx@gmail.com>
2024-01-19 17:04:56 -0500

    commit

--- src/Globals.java
+++ src/Globals.java
@@ -3,8 +3,8 @@ public class Globals {
     public static final int SCREEN_WIDTH = 1250;
     public static final int SCREEN_HEIGHT = 800;
 
-    public static final int floor = SCREEN_HEIGHT-150;
-
+//    public static final int floor = SCREEN_HEIGHT-150;
+    public static final int floor = SCREEN_HEIGHT-50;
     public static final int gridWidth = 75;
     public static final int slabWidth = 75;
     public static final int slabHeight = 37;
